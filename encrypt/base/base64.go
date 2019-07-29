9f1544bf55f78fc3e383492aec9631267c08f083
Vanilla <rose20.99.c@gmail.com>
2019-07-29 18:36:14 +0800

    [update] f

---
+++ encrypt/base/base64.go
@@ -0,0 +1,21 @@
+package base
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func Example() {
+	msg := "Hello, 世界"
+	encoded := base64.StdEncoding.EncodeToString([]byte(msg))
+	fmt.Println(encoded)
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("decode error:", err)
+		return
+	}
+	fmt.Println(string(decoded))
+	// Output:
+	// SGVsbG8sIOS4lueVjA==
+	// Hello, 世界
+}
