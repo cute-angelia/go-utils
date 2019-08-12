45e5e849b2c9a434540813f03e5e62968908b736
Vanilla <rose20.99.c@gmail.com>
2019-08-12 15:41:17 +0800

    [update]  postgres

---
+++ umysql/gormPg.go
@@ -0,0 +1,76 @@
+package umysql
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"log"
+	"time"
+	"github.com/smacker/opentracing-gorm"
+)
+
+// gorm 连接池, gorm 本身也提供了pool, 提供全局对象, 这里初始化全局对象
+var GormPg = make(map[string]*gorm.DB)
+// 配置, 为了断开重连
+var GormPgOpts = make(map[string]GormOptions)
+
+// 初始化 GORM
+func InitGormPg(opts GormOptions) {
+	orm, ok := GormPg[opts.Dbname]
+	if !ok {
+		orm = initPgDB(opts)
+		GormPg[opts.Dbname] = orm
+
+		GormPgOpts[opts.Dbname] = opts
+	}
+}
+
+// 获取 Gorm
+func GetGormPg(dbname string) *gorm.DB {
+	db, ok := GormPg[dbname]
+	if !ok {
+		log.Println("没有初始化" + dbname)
+	} else {
+		err := db.DB().Ping()
+		if err != nil {
+			GormPg[dbname] = initPgDB(GormPgOpts[dbname])
+		}
+	}
+	return GormPg[dbname]
+}
+
+//  内部初始化 DB
+func initPgDB(opts GormOptions) *gorm.DB {
+	var (
+		db  *gorm.DB
+		err error
+	)
+
+	// config
+	ocosMax := opts.ConnMax
+	dbType := "postgres"
+
+	connectString := ""
+	connectString = opts.Dsn
+
+	//db, err := gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
+	//defer db.Close()
+
+	for db, err = gorm.Open(dbType, connectString); err != nil; {
+		fmt.Println("数据库连接异常! 5秒重试")
+		log.Println("数据库连接异常", err)
+		time.Sleep(5 * time.Second)
+		db, err = gorm.Open(dbType, connectString)
+	}
+
+	//最大打开连接数
+	db.DB().SetMaxIdleConns(ocosMax)
+	db.DB().SetMaxOpenConns(ocosMax * 10)
+
+	// LogMode Print log
+	db.LogMode(opts.LogDebug)
+
+	// 设置 callback
+	otgorm.AddGormCallbacks(db)
+
+	return db
+}
