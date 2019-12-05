7c2db08c22d0783e25d71d4fb9b1fb4f8dcc2fea
Vanilla <rose20.99.c@gmail.com>
2019-12-05 19:05:48 +0800

    [update] fix uuid

--- generator/snowflake/index.go
+++ generator/snowflake/index.go
@@ -7,19 +7,52 @@ import (
 	"net"
 	"errors"
 	"fmt"
+	"strconv"
 )
 
-func NewSnowId(machineId bool) (id uint64, error error) {
-	var st sonyflake.Settings
-	if machineId {
-		st.MachineID = MachineIP
-	}
-	sf := sonyflake.NewSonyflake(st)
+var st sonyflake.Settings
+var sf *sonyflake.Sonyflake
+
+func init() {
+	sf = sonyflake.NewSonyflake(st)
+}
+
+type SnowRest struct {
+	Id uint64
+}
+
+// Int64 returns an int64 of the snowflake ID
+func (f SnowRest) Int64() int64 {
+	return int64(f.Id)
+}
+
+// String returns a string of the snowflake ID
+func (f SnowRest) String() string {
+	return strconv.FormatInt(int64(f.Id), 10)
+}
+
+func NewSnowId(mode int64) (SnowRest, error) {
+	z := SnowRest{}
+
 	if sf == nil {
-		return 0, fmt.Errorf("sonyflake not created")
+		return z, fmt.Errorf("sonyflake not created")
+	}
+
+	id, err := sf.NextID()
+	z.Id = id
+	return z, err
+}
+
+func NewSnowIdWithSetting(st sonyflake.Settings) (SnowRest, error) {
+	sf := sonyflake.NewSonyflake(st)
+	z := SnowRest{}
+
+	if sf == nil {
+		return z, fmt.Errorf("sonyflake not created")
 	}
 	id, err := sf.NextID()
-	return id, err
+	z.Id = id
+	return z, err
 }
 
 func EC2PrivateIPv4() (net.IP, error) {
