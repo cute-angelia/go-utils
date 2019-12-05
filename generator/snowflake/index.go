15183654e07c8c8475014cb39137dae958a70b6e
Vanilla <rose20.99.c@gmail.com>
2019-12-05 18:45:01 +0800

    [update]

--- generator/snowflake/index.go
+++ generator/snowflake/index.go
@@ -1,20 +1,51 @@
 package snowflake
 
 import (
+	"github.com/sony/sonyflake"
+	"io/ioutil"
+	"net/http"
+	"net"
+	"errors"
 	"fmt"
-	snowflakeLib "github.com/bwmarrin/snowflake"
 )
 
-func NewSnowId(nodeId int64) (snowflakeLib.ID, error) {
-	// Create a new Node with a Node number of 1
-	node, err := snowflakeLib.NewNode(nodeId)
+func NewSnowId(machineId bool) (id uint64, error error) {
+	var st sonyflake.Settings
+	if machineId {
+		st.MachineID = MachineIP
+	}
+	sf := sonyflake.NewSonyflake(st)
+	if sf == nil {
+		return 0, fmt.Errorf("sonyflake not created")
+	}
+	id, err := sf.NextID()
+	return id, err
+}
+
+func EC2PrivateIPv4() (net.IP, error) {
+	res, err := http.Get("https://ident.me")
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	ip := net.ParseIP(string(body))
+	if ip == nil {
+		return nil, errors.New("invalid ip address")
+	}
+	return ip.To4(), nil
+}
+
+func MachineIP() (uint16, error) {
+	ip, err := EC2PrivateIPv4()
+	if err != nil {
 		return 0, err
 	}
 
-	// Generate a snowflake ID.
-	id := node.Generate()
-
-	return id, nil
+	return uint16(ip[2])<<8 + uint16(ip[3]), nil
 }
