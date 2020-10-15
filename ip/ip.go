774effb8a9252edf43b741a48e362b59e8763b26
angelia <rose20.99.c@gmail.com>
2020-10-15 11:15:41 +0800

    add ip

--- ip/ip.go
+++ ip/ip.go
@@ -1,7 +1,10 @@
 package ip
 
 import (
+	"encoding/binary"
 	"log"
+	"net"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -34,4 +37,47 @@ func GetInfo(ip string) map[string]ResultQQwry {
 	}
 
 	return rs
+}
+
+// RemoteIp 返回远程客户端的 IP，如 192.168.1.1
+func RemoteIp(req *http.Request) string {
+	remoteAddr := req.RemoteAddr
+	if ip := req.Header.Get("X-Real-IP"); ip != "" {
+		remoteAddr = ip
+	} else if ip = req.Header.Get("X-Forwarded-For"); ip != "" {
+		remoteAddr = ip
+	} else {
+		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
+	}
+
+	if remoteAddr == "::1" {
+		remoteAddr = "127.0.0.1"
+	}
+	return remoteAddr
+}
+
+func RemoteIp2Uint(req *http.Request) uint32 {
+	remoteAddr := req.RemoteAddr
+	if ip := req.Header.Get("X-Real-IP"); ip != "" {
+		remoteAddr = ip
+	} else if ip = req.Header.Get("X-Forwarded-For"); ip != "" {
+		remoteAddr = ip
+	} else {
+		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
+	}
+
+	if remoteAddr == "::1" {
+		remoteAddr = "127.0.0.1"
+	}
+	return Ip2long(remoteAddr)
+}
+
+// Ip2long 将 IPv4 字符串形式转为 uint32
+func Ip2long(ipstr string) uint32 {
+	ip := net.ParseIP(ipstr)
+	if ip == nil {
+		return 0
+	}
+	ip = ip.To4()
+	return binary.BigEndian.Uint32(ip)
 }
