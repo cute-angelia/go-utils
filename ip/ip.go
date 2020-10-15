5b4e1f3503e22e67eca4df7fb9b6a6d7063a9550
angelia <rose20.99.c@gmail.com>
2020-10-15 11:44:19 +0800

    add ip

--- ip/ip.go
+++ ip/ip.go
@@ -81,3 +81,10 @@ func Ip2long(ipstr string) uint32 {
 	ip = ip.To4()
 	return binary.BigEndian.Uint32(ip)
 }
+
+func Long2ip(ipLong uint32) string {
+	ipByte := make([]byte, 4)
+	binary.BigEndian.PutUint32(ipByte, ipLong)
+	ip := net.IP(ipByte)
+	return ip.String()
+}
