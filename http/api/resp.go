75b7ca7d229e73a5b1f60126c682d8ab741f9e4d
vanilla <vanilla@Vanilla-MBP.lan>
2020-12-17 16:16:50 +0800

    v1.2.4

--- http/api/resp.go
+++ http/api/resp.go
@@ -1,20 +1,28 @@
 package api
 
 import (
-	"net/http"
 	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 )
+
+var LogRequestAndData = false
 
 func Success(w http.ResponseWriter, r *http.Request, data interface{}, msg string) {
 	response := map[string]interface{}{
-		"code": 0,
-		"message":  msg,
-		"data": data,
+		"code":    0,
+		"message": msg,
+		"data":    data,
 	}
 
 	// 日志
 	// FIXED 优化,协程处理
 	// ApiMakeLog.createLog(r, response)
+	if LogRequestAndData {
+		logr(r, response)
+	}
 
 	// json
 	w.Header().Set("Content-Type", "application/json")
@@ -26,8 +34,8 @@ func Success(w http.ResponseWriter, r *http.Request, data interface{}, msg strin
 }
 
 /**
-	错误
- */
+错误
+*/
 func Error(w http.ResponseWriter, r *http.Request, data interface{}, msg string, code int32) {
 	// 内部错误
 	if code == 500 {
@@ -36,9 +44,13 @@ func Error(w http.ResponseWriter, r *http.Request, data interface{}, msg string,
 	}
 
 	response := map[string]interface{}{
-		"code": code,
-		"message":  msg,
-		"data": nil,
+		"code":    code,
+		"message": msg,
+		"data":    nil,
+	}
+
+	if LogRequestAndData {
+		logr(r, response)
 	}
 
 	// 日志
@@ -51,4 +63,36 @@ func Error(w http.ResponseWriter, r *http.Request, data interface{}, msg string,
 		http.Error(w, err.Error(), 500)
 		return
 	}
+}
+
+func logr(r *http.Request, response interface{}) {
+	// 打印日志
+	go func() {
+		z, _ := json.Marshal(r.PostForm)
+		z2, _ := json.Marshal(response)
+		zuid := r.Header.Get("jwt_uid")
+
+		log.Println("------------------------------------------------------------------------------")
+		log.Printf("用户: %s, 请求地址: %s?%s", zuid, r.URL.Path, r.URL.RawQuery)
+
+		jwt_app_start_time := r.Header.Get("jwt_app_start_time")
+		if len(jwt_app_start_time) > 0 {
+			un, _ := strconv.Atoi(jwt_app_start_time)
+			t2 := time.Unix(int64(un), 0)
+			tc := time.Since(t2)
+
+			flags := "Millisecond"
+			if tc < time.Second {
+				tc = tc / 10
+			} else {
+				flags = "Second"
+			}
+
+			log.Printf("用户: %s, TimeCost: %v %s", zuid, tc, flags)
+		}
+
+		log.Printf("用户: %s, 请求数据: %s,", zuid, z)
+		log.Printf("用户: %s, 响应数据: %s", zuid, z2)
+		log.Println("------------------------------------------------------------------------------")
+	}()
 }
