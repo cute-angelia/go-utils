f8a0dee38da403a84848746a7cd894641ce8a753
vanilla <rose20.99.c@gmail.com>
2021-03-10 15:53:24 +0800

    update

--- utils/qcloud/robot/component.go
+++ utils/qcloud/robot/component.go
@@ -26,6 +26,17 @@ func (self Component) SendText(content string) {
 	}).Debug(self.config.Debug).Do()
 }
 
+func (self Component) SendMarkDown(content string) {
+	gout.POST(self.config.Uri).SetJSON(gout.H{
+		"msgtype": "markdown",
+		"markdown": gout.H{
+			"content":               content,
+			"mentioned_list":        self.config.MentionedList,
+			"mentioned_mobile_list": self.config.MentionedMobileList,
+		},
+	}).Debug(self.config.Debug).Do()
+}
+
 func logError(key string, err error) {
 	log.Println(PackageName, ":error:"+key+":", err)
 }
