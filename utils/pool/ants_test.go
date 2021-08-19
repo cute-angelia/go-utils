e0e795aaa1db9c166041ff5f3125cc430a720851
one.cat <rose1988.cat@gmail.com>
2021-08-19 14:59:48 +0800

    update

--- utils/pool/ants_test.go
+++ utils/pool/ants_test.go
@@ -1,23 +1,45 @@
 package pool
 
 import (
+	"log"
 	"testing"
 	"time"
 )
 
 func TestAnts(t *testing.T) {
-	p := NewPoolAnts(100)
-	p.InitTask(10)
-	for i := 0; i < 10; i++ {
 
+	p := NewPoolAnts(100)
+	defer p.Stop()
+
+	generate(p)
+
+	time.Sleep(time.Second * 1)
+}
+
+func generate(p *AntsPool) {
+
+	as := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	p.InitTask(10 + len(as))
+
+	for _, i2 := range as {
+		// notice 必须重新赋值
+		newi2 := i2
+		p.SubmitTask(func() {
+			dosomethings("--> stp1", newi2)
+		})
+	}
+
+	for i := 0; i < 10; i++ {
 		// pool 一些都会重新赋值
 		z := i
 		p.SubmitTask(func() {
-			t.Log("task:", z)
+			dosomethings("stp2", z)
 		})
 	}
-	t.Log("RunningTask", p.RunningTask())
-	defer p.stop()
+	log.Println("RunningTask", p.RunningTask())
+}
 
-	time.Sleep(time.Second*1)
+func dosomethings(flag string, z int) {
+	log.Println("task:", flag, z)
 }
