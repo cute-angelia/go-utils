87befbc4369394cd10e5f378bfbf373eb3ad7729
vanilla <rose20.99.c@gmail.com>
2021-11-15 10:01:25 +0800

    update

--- Makefile
+++ Makefile
@@ -12,6 +12,6 @@ tag:
 	git add .
 	git commit -am "update"
 	git push origin master
-	git tag v1.6.55
+	git tag v1.6.56
 	git push --tags
 	@echo "\n tags 发布中..."
--- syntax/ierror/fmt.go
+++ syntax/ierror/fmt.go
@@ -7,15 +7,17 @@ import (
 type Err struct {
 	Code int
 	Msg  string
+	Data interface{}
 }
 
 func (e *Err) Error() string {
 	err, _ := ijson.Marshal(e)
 	return string(err)
 }
-func New(code int, msg string) *Err {
+func New(code int, msg string, data interface{}) *Err {
 	return &Err{
 		Code: code,
 		Msg:  msg,
+		Data: data,
 	}
 }
