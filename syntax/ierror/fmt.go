13523a1184a20c41c81a166fe76394b06e2d18a6
vanilla <rose20.99.c@gmail.com>
2021-11-16 16:34:27 +0800

    update

--- syntax/ierror/fmt.go
+++ syntax/ierror/fmt.go
@@ -5,19 +5,29 @@ import (
 )
 
 type Err struct {
-	Code int
-	Msg  string
-	Data interface{}
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
 }
 
 func (e *Err) Error() string {
 	err, _ := ijson.Marshal(e)
 	return string(err)
 }
+
 func New(code int, msg string, data interface{}) *Err {
 	return &Err{
 		Code: code,
 		Msg:  msg,
 		Data: data,
 	}
+}
+
+func Decode(str string) Err {
+	e := Err{}
+	if err := ijson.Unmarshal([]byte(str), &e); err != nil {
+		e.Code = -1
+		e.Msg = str + " + 解析失败"
+	}
+	return e
 }
