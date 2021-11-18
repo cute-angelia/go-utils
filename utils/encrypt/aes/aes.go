c33da4c91030df3f90b4e07dbe5f4924dff042e9
vanilla <rose20.99.c@gmail.com>
2021-11-18 09:53:31 +0800

    update

--- utils/encrypt/aes/aes.go
+++ utils/encrypt/aes/aes.go
@@ -1,19 +1,34 @@
 package aes
 
+/*
+ [wiki](https://zh.wikipedia.org/wiki/%E5%88%86%E7%BB%84%E5%AF%86%E7%A0%81%E5%B7%A5%E4%BD%9C%E6%A8%A1%E5%BC%8F)
+（1）AES有5种加密模式，分别是：
+ （a）电子密码本（Electronic Codebook Book，ECB）； 照块密码的块大小被分为数个块，并对每个块进行独立加密, 不能很好的隐藏数据模式
+ （b）密码块链接（Cipher Block Chaining ，CBC），如果明文长度不是分组长度16字节的整数倍需要进行填充； CBC是最为常用的工作模式 加密串行，解密并行
+ （c）计数器模式（Counter，CTR）；
+ （d）密文反馈（Cipher FeedBack，CFB）； 模式类似于反向 CBC
+ （e）输出反馈（Output FeedBack，OFB）。 然后将其与明文块进行异或，得到密文
+（2）AES是对称分组加密算法，每组长度为128bits，即16字节。
+（3）AES秘钥的长度只能是16、24或32字节，分别对应三种AES，即AES-128, AES-192和AES-256，三者的区别是加密的轮数不同；
+
+块密码只能对确定长度的数据块进行处理，而消息的长度通常是可变的。因此部分模式（即ECB和CBC）需要最后一块在加密前进行填充。
+*/
+
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"encoding/base64"
-	"io"
 	"crypto/rand"
+	"encoding/base64"
 	"errors"
+	"io"
 )
 
 func GetAesKey() []byte {
 	return []byte("passphrasewhichneedstobe32bytes.")
 }
 
-func Encrypt(key []byte, message string) (encmess string, err error) {
+func EncryptCFB(key []byte, message string) (encmess string, err error) {
 	plainText := []byte(message)
 
 	block, err := aes.NewCipher(key)
@@ -37,7 +52,7 @@ func Encrypt(key []byte, message string) (encmess string, err error) {
 	return
 }
 
-func Decrypt(key []byte, securemess string) (decodedmess string, err error) {
+func DecryptCFB(key []byte, securemess string) (decodedmess string, err error) {
 	cipherText, err := base64.URLEncoding.DecodeString(securemess)
 	if err != nil {
 		return
@@ -64,4 +79,50 @@ func Decrypt(key []byte, securemess string) (decodedmess string, err error) {
 
 	decodedmess = string(cipherText)
 	return
+}
+
+//@brief:AES加密 CBC
+func EncryptCBC(origData, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//AES分组长度为128位，所以blockSize=16，单位字节
+	blockSize := block.BlockSize()
+	origData = PKCS5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize]) //初始向量的长度必须等于块block的长度16字节
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+//@brief:AES解密 CBC
+func DecryptCBC(crypted, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//AES分组长度为128位，所以blockSize=16，单位字节
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) //初始向量的长度必须等于块block的长度16字节
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	return origData, nil
+}
+
+//@brief:填充明文
+func PKCS5Padding(plaintext []byte, blockSize int) []byte {
+	padding := blockSize - len(plaintext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(plaintext, padtext...)
+}
+
+//@brief:去除填充数据
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
 }
