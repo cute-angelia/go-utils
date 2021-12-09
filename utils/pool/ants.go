70c0fa6b20a6abdb034f5fe919dd4c8955262328
vanilla <rose20.99.c@gmail.com>
2021-12-09 10:36:22 +0800

    update

--- utils/pool/ants.go
+++ utils/pool/ants.go
@@ -3,53 +3,48 @@ package pool
 import (
 	"fmt"
 	"github.com/panjf2000/ants/v2"
+	"log"
 	"sync"
 )
 
 type AntsPool struct {
-	pool    *ants.Pool
-	wg      *sync.WaitGroup
-	taskNum int
+	pool *ants.Pool
+	wg   *sync.WaitGroup
 }
 
-// num 协程数量
-func NewPoolAnts(num int) *AntsPool {
+// num 协程数量 , 预先分配内存
+func MustNewPoolAnts(num int, withPreAlloc bool) *AntsPool {
 	if num <= 0 {
 		panic(fmt.Errorf("需要制定协程数量"))
 	}
-
 	ap := AntsPool{}
+	var wg sync.WaitGroup
 	// 预先分配内存 WithPreAlloc
-	if p, err := ants.NewPool(num, ants.WithPreAlloc(true)); err != nil {
+	if p, err := ants.NewPool(num, ants.WithPreAlloc(withPreAlloc)); err != nil {
 		panic(err)
 	} else {
 		ap.pool = p
+		ap.wg = &wg
 	}
-
 	return &ap
 }
 
-// 步骤 1  任务数量
-func (t *AntsPool) InitTask(taskNum int) {
-	var wg sync.WaitGroup
-
-	// 检查数量
-	t.taskNum = taskNum
-	if taskNum > 0 {
-		wg.Add(taskNum)
-	}
-
-	t.wg = &wg
-}
+//// 步骤 1  任务数量
+//func (t *AntsPool) WgAdd() *AntsPool {
+//	t.wg.Add(1)
+//	return t
+//}
 
 // 步骤 2
 func (t *AntsPool) SubmitTask(fc func()) error {
-	if t.taskNum <= 0 {
-		return fmt.Errorf("you should InitTask, 需要指定任务数量")
-	} else {
-		err := t.pool.Submit(fc)
+	t.wg.Add(1)
+	if err := t.pool.Submit(fc); err != nil {
+		log.Println(err)
 		t.wg.Done()
 		return err
+	} else {
+		t.wg.Done()
+		return nil
 	}
 }
 
--- utils/pool/ants_test.go
+++ utils/pool/ants_test.go
@@ -7,39 +7,17 @@ import (
 )
 
 func TestAnts(t *testing.T) {
-
-	p := NewPoolAnts(100)
+	p := MustNewPoolAnts(2, false)
 	defer p.Stop()
 
-	generate(p)
-
-	time.Sleep(time.Second * 1)
-}
-
-func generate(p *AntsPool) {
-
 	as := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	p.InitTask(10 + len(as))
-
 	for _, i2 := range as {
 		// notice 必须重新赋值
 		newi2 := i2
 		p.SubmitTask(func() {
-			dosomethings("--> stp1", newi2)
-		})
-	}
-
-	for i := 0; i < 10; i++ {
-		// pool 一些都会重新赋值
-		z := i
-		p.SubmitTask(func() {
-			dosomethings("stp2", z)
+			log.Println("--> ", newi2)
 		})
 	}
 	log.Println("RunningTask", p.RunningTask())
-}
-
-func dosomethings(flag string, z int) {
-	log.Println("task:", flag, z)
+	<-time.After(time.Second * 1)
 }
