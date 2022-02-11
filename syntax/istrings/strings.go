8a74cf4c80ec250b5553bab2b4d6d79c51be8e04
vanilla <rose20.99.c@gmail.com>
2022-02-11 14:36:42 +0800

    update

--- syntax/istrings/strings.go
+++ syntax/istrings/strings.go
@@ -48,3 +48,15 @@ func StringInSlice(a string, list []string) bool {
 	}
 	return false
 }
+
+// Cut slices s around the first instance of sep,
+// returning the text before and after sep.
+// The found result reports whether sep appears in s.
+// If sep does not appear in s, cut returns s, "", false.
+// 1.18 已支持，这里兼容旧版 不方便升级的包
+func Cut(s, sep string) (before, after string, found bool) {
+	if i := strings.Index(s, sep); i >= 0 {
+		return s[:i], s[i+len(sep):], true
+	}
+	return s, "", false
+}
