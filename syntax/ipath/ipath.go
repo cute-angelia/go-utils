2a454fb4f06e82578e4adfd6c4787a20316ead23
van <no-replay@xgmail.com>
2023-08-31 16:08:26 +0800

    update

--- syntax/ipath/ipath.go
+++ syntax/ipath/ipath.go
@@ -14,7 +14,7 @@ func Clean(p string) string {
 // GetFileName 根据路径获取文件名
 func GetFileName(filePath string) (name string) {
 	filePath = Clean(filePath)
-	ext := path.Ext(filePath)
+	ext := strings.ToLower(path.Ext(filePath))
 	name = path.Base(filePath[:len(filePath)-len(ext)])
 	return
 }
@@ -22,7 +22,7 @@ func GetFileName(filePath string) (name string) {
 // GetFileNameAndExt 根据路径获取文件名 和 后缀
 func GetFileNameAndExt(filePath string) (name string, ext string) {
 	filePath = Clean(filePath)
-	ext = path.Ext(filePath)
+	ext = strings.ToLower(path.Ext(filePath))
 	name = path.Base(filePath[:len(filePath)-len(ext)])
 	return
 }
@@ -30,7 +30,7 @@ func GetFileNameAndExt(filePath string) (name string, ext string) {
 // GetFileExt 后缀
 func GetFileExt(filePath string) (ext string, found bool) {
 	filePath = Clean(filePath)
-	ext = path.Ext(filePath)
+	ext = strings.ToLower(path.Ext(filePath))
 	if strings.Contains(ext, ".") {
 		return ext, true
 	} else {
