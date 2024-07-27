aaa9581b5bd51adcc9c9ae8f3ea85c4b9ae737a5
cute-angelia <rose20.99.c@gmail.com>
2024-07-27 20:23:37 +0800

    update

--- syntax/icmd/cmd.go
+++ syntax/icmd/cmd.go
@@ -6,6 +6,29 @@ import (
 	"time"
 )
 
+/*
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-cmd/cmd"
+)
+
+func main() {
+	// Create Cmd, buffered output
+	envCmd := cmd.NewCmd("env")
+
+	// Run and wait for Cmd to return Status
+	status := <-envCmd.Start()
+
+	// Print each line of STDOUT from Cmd
+	for _, line := range status.Stdout {
+		fmt.Println(line)
+	}
+}
+*/
+
 func Exec(exePath string, param []string, timeout time.Duration) cmd.Status {
 	findCmd := cmd.NewCmd(exePath, param...)
 	statusChan := findCmd.Start() // non-blocking
