6eaae0a28aad8a8220c85a81010aaa734e238d36
cute-angelia <rose20.99.c@gmail.com>
2024-11-08 10:45:09 +0800

    update

---
+++ syntax/icall/call.go
@@ -0,0 +1,29 @@
+package icall
+
+import (
+	"log"
+	"runtime"
+)
+
+func Call(fn func()) {
+	if fn == nil {
+		return
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			switch err.(type) {
+			case runtime.Error:
+				log.Panic(err)
+			default:
+				log.Panicf("panic error: %v", err)
+			}
+		}
+	}()
+
+	fn()
+}
+
+func Go(fn func()) {
+	go Call(fn)
+}
--- syntax/inet/ip.go
+++ syntax/inet/ip.go
@@ -1,24 +1,177 @@
 package inet
 
-import "net"
+import (
+	"encoding/binary"
+	"errors"
+	"net"
+	"strconv"
+)
 
-// InternalIP get internal IP
-func InternalIP() (ip string) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		panic("Oops: " + err.Error())
+// ParseAddr 解析地址
+func ParseAddr(addr string) (listenAddr, exposeAddr string, err error) {
+	var host, port string
+
+	if addr != "" {
+		host, port, err = net.SplitHostPort(addr)
+		if err != nil {
+			return
+		}
 	}
 
-	for _, a := range addrs {
-		if ipNet, ok := a.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
-			if ipNet.IP.To4() != nil {
-				// os.Stdout.WriteString(ipNet.IP.String() + "\n")
-				ip = ipNet.IP.String()
-				return
+	if port == "" || port == "0" {
+		p, err := AssignRandPort(host)
+		if err != nil {
+			return "", "", err
+		}
+		port = strconv.Itoa(p)
+	}
+
+	if len(host) > 0 && (host != "0.0.0.0" && host != "[::]" && host != "::") {
+		listenAddr = net.JoinHostPort(host, port)
+		exposeAddr = listenAddr
+	} else {
+		ip, err := InternalIP()
+		if err != nil {
+			return "", "", err
+		}
+		listenAddr = net.JoinHostPort("0.0.0.0", port)
+		exposeAddr = net.JoinHostPort(ip, port)
+	}
+
+	return
+}
+
+// ExtractIP 提取主机地址
+func ExtractIP(addr net.Addr) (ip string, err error) {
+	ip, _, err = net.SplitHostPort(addr.String())
+	return
+}
+
+// ExtractPort 提取主机端口
+func ExtractPort(addr net.Addr) (int, error) {
+	_, port, err := net.SplitHostPort(addr.String())
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(port)
+}
+
+// InternalIP 获取内网IP地址
+func InternalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	var (
+		addrs []net.Addr
+		ipnet net.IP
+		ip    string
+	)
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		if addrs, err = iface.Addrs(); err != nil {
+			return "", err
+		}
+
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ipnet = v.IP
+			case *net.IPAddr:
+				ipnet = v.IP
+			default:
+				err = errors.New("invalid addr interface")
+				continue
+			}
+
+			if ipnet == nil || ipnet.IsLoopback() {
+				continue
+			}
+
+			if ipv4 := ipnet.To4(); ipv4 != nil && ipv4.IsPrivate() {
+				if ipv4[0] == 192 && ipv4[1] == 168 {
+					return ipv4.String(), nil
+				}
+
+				if ip == "" {
+					ip = ipv4.String()
+				}
 			}
 		}
 	}
 
-	// os.Exit(0)
-	return
+	if ip != "" {
+		return ip, nil
+	} else {
+		return "", errors.New("not found ip address")
+	}
+}
+
+// ExternalIP 获取外网IP地址
+func ExternalIP() (string, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:54")
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	return ExtractIP(conn.LocalAddr())
+}
+
+// AssignRandPort 分配一个随机端口
+func AssignRandPort(ip ...string) (int, error) {
+	addr := ":0"
+	if len(ip) > 0 {
+		addr = ip[0] + addr
+	}
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+
+	port := listener.Addr().(*net.TCPAddr).Port
+
+	_ = listener.Close()
+
+	return port, nil
+}
+
+// FulfillAddr 补全地址
+func FulfillAddr(addr string) string {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return addr
+	}
+	if host == "" {
+		host = "0.0.0.0"
+	}
+
+	return net.JoinHostPort(host, port)
+}
+
+// IP2Long IP地址转换为长整型
+func IP2Long(ip string) uint32 {
+	v := net.ParseIP(ip).To4()
+
+	if len(v) == 0 {
+		return 0
+	}
+
+	return binary.BigEndian.Uint32(v)
+}
+
+// Long2IP 长整型转换为字符串地址
+func Long2IP(v uint32) string {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, v)
+	return ip.String()
 }
